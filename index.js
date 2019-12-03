0b488969940ef051eb44b88b71f98d6236775568
KHong <albtrossfacefall@gmail.com>
2019-12-03 15:16:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string) {
+  return array.filter(n => {
+    return n.toLowerCase() === string.toLowerCase()
+  });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(name => {
+    return name.startsWith(string)
+  });
+}
+
+function matchName(array, string) {
+  return array.filter(driver => {
+    return driver.name === string
+  })
+}
